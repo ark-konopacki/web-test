c4865ccfb40040b30e123ed41b36920cc741cd8c
Przemysław Urkiel <przemyslaw.urkiel@gmail.com>
2016-10-10 22:15:50 +0200

    Small fix in test

--- src/main/java/pl/webtest/page/google/SearchResultsPage.java
+++ src/main/java/pl/webtest/page/google/SearchResultsPage.java
@@ -1,33 +1,30 @@
 package pl.webtest.page.google;
 
-import java.util.List;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-
 import pl.webtest.page.Page;
+
+import java.util.List;
 
 /**
  * Google search results page
  */
 public class SearchResultsPage extends Page {
 
-	@FindBy(name = "ires")
-	private WebElement results;
-	
+    @FindBy(css = "#ires .srg a")
+    private List<WebElement> results;
+
     public SearchResultsPage(WebDriver webDriver) {
         super(webDriver);
     }
 
-	public String getTopResultTitle() {
-		String topResultTitle = "";
-		List<WebElement> results = webDriver.findElements(By.cssSelector("#ires .srg a"));
-		if(results.size() > 0 ) {
-			topResultTitle =  results.get(0).getText();
-		}
-		
-		return topResultTitle;
-	}
+    public String getTopResultTitle() {
+        String topResultTitle = "";
+        if (results.size() > 0) {
+            topResultTitle = results.get(0).getText();
+        }
+
+        return topResultTitle;
+    }
 }
--- src/main/java/pl/webtest/util/WebDriverUtils.java
+++ src/main/java/pl/webtest/util/WebDriverUtils.java
@@ -1,10 +1,5 @@
 package pl.webtest.util;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -14,43 +9,49 @@ import org.openqa.selenium.remote.ScreenshotException;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import pl.webtest.Config;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 /**
  * Webdriver utilities
  */
 public class WebDriverUtils {
-	private static final Logger logger = LoggerFactory.getLogger(WebDriverUtils.class);
-	
-    private WebDriverUtils() {}
-	
+    private static final Logger logger = LoggerFactory.getLogger(WebDriverUtils.class);
+
+    private WebDriverUtils() {
+    }
+
     /**
-     *  Save screen shot method
+     * Save screen shot method
+     *
      * @param webDriver
      * @param screenshotName name of the screenshot. It will be saved under @code Config.SCREENSHOT_FOLDER location.
      * @return created screenshot file. null will be returned if the were some problems with creating the screenshot file.
      */
     public static Path saveScreenshot(WebDriver webDriver, String screenshotName) {
-    	Path dest = null;
+        Path dest = null;
         try {
-        	WebDriver returned;
-			if (webDriver instanceof EventFiringWebDriver) {
-				// Unwrap real webdriver
-				EventFiringWebDriver eventFiringWebDriver = (EventFiringWebDriver)webDriver;
-				webDriver = eventFiringWebDriver.getWrappedDriver();
-			}       	
-        	if(webDriver.getClass().isAnnotationPresent(Augmentable.class)) {
-        		//Augmenter can only be called on RemoteWebDriver and none of the child classes. See https://code.google.com/p/selenium/issues/detail?id=5087
-        		returned = new Augmenter().augment(webDriver);        		
-        	} else {
-        		returned = webDriver;
-        	}
-        	
+            WebDriver returned;
+            if (webDriver instanceof EventFiringWebDriver) {
+                // Unwrap real webdriver
+                EventFiringWebDriver eventFiringWebDriver = (EventFiringWebDriver) webDriver;
+                webDriver = eventFiringWebDriver.getWrappedDriver();
+            }
+            if (webDriver.getClass().isAnnotationPresent(Augmentable.class)) {
+                //Augmenter can only be called on RemoteWebDriver and none of the child classes. See https://code.google.com/p/selenium/issues/detail?id=5087
+                returned = new Augmenter().augment(webDriver);
+            } else {
+                returned = webDriver;
+            }
+
             if (returned != null) {
                 File src = ((TakesScreenshot) returned).getScreenshotAs(OutputType.FILE);
                 try {
-                	dest = getScreenshotFile(screenshotName);
+                    dest = getScreenshotFile(screenshotName);
                     Files.createDirectories(dest.getParent());
                     Files.copy(src.toPath(), dest);
                     logger.debug("Screenshot saved to " + dest.toAbsolutePath());
@@ -59,9 +60,9 @@ public class WebDriverUtils {
                 }
             }
         } catch (ScreenshotException e) {
-        	logger.error("Error taking screenshot", e);
+            logger.error("Error taking screenshot", e);
         }
-        
+
         return dest;
     }
 
